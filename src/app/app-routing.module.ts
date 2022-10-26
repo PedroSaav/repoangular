cf74e21268365ba38cbab4e35abd0fef8aa05760
Pedro <pepitosk8hd@gmail.com>
2022-10-26 16:24:44 -0300

    testing angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './componentes/paginas/about/about.component';
+import { BlogComponent } from './componentes/paginas/blog/blog.component';
+import { ContactComponent } from './componentes/paginas/contact/contact.component';
+import { MainComponent } from './componentes/paginas/main/main.component';
+import { MasonComponent } from './componentes/paginas/mason/mason.component';
+
+
+const routes: Routes = [
+    { path: 'main', component: MainComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'contact', component: ContactComponent} ,
+    { path: 'mason', component: MasonComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'main' },
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes,{ useHash: true ,scrollPositionRestoration: 'enabled'})],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
